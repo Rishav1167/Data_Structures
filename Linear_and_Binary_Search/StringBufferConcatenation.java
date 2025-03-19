8d7787b852e948ae0f2d89ad60c5c5e0df70cd2f
Rishav1167 <Rishav1167>
2025-03-19 09:52:37 +0530

    [Rishav] Add:Read a File Line by Line Using FileReader

--- Linear_and_Binary_Search/StringBufferConcatenation.java
+++ Linear_and_Binary_Search/StringBufferConcatenation.java
@@ -1,4 +1,17 @@
 package Linear_and_Binary_Search;
 
 public class StringBufferConcatenation {
+    public static String concatenateStrings(String[] strings) {
+        StringBuffer sb = new StringBuffer();
+        for (String s : strings) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"Hello", " ", "World", "!"};
+        String result = concatenateStrings(words);
+        System.out.println(result);
+    }
 }
