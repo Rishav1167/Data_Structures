e131442cfd6ba9f38366a96a1171787693105e31
Rishav1167 <Rishav1167>
2025-03-21 09:31:55 +0530

    [Rishav] Add:   Search for the First Negative Number

--- Linear_and_Binary_Search/FirstNegative.java
+++ Linear_and_Binary_Search/FirstNegative.java
@@ -1,4 +1,18 @@
 package Linear_and_Binary_Search;
 
-public class FirstNegative {
+public class FirstNegative{
+
+    public static int FirstNegative(int[] arr){
+        for (int i = 0; i < arr.length; i++){
+            if (arr[i] < 0) return arr[i];
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int[] arr = {29, 324, 525, 661, -33, 313};
+
+        System.out.println(FirstNegative(arr));
+    }
 }
